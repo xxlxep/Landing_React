39419b8819a454fab1711f32bae8653df4a0c735
xxlxep <xxlxep@gmail.com>
2019-02-28 15:46:39 +0300

    featured component

---
+++ src/components/featured/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Carrousel from './Carrousel';
+import TimeUntill from './TimeUntill';
+
+const Featured = () => {
+    return (
+        <div style={{position:'relative'}}>
+            
+            <Carrousel />
+
+            <div className="artist_name">
+                <div className="wrapper">
+                    Ariana Grande
+                </div>
+            </div>
+
+            <TimeUntill />
+        </div>
+    );
+};
+
+export default Featured;
