20291584b39a12a287b19686e44e610fd4a57d1e
xxlxep <xxlxep@gmail.com>
2019-03-01 20:59:27 +0300

    VenueNfo component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
+import VenueNfo from './components/venueNfo';
+
 
 class App extends Component {
   render() {
@@ -9,6 +11,7 @@ class App extends Component {
       <div className="App" style={{ height:"1500px", background:'cornflowerblue'}}>
          <Header />
          <Featured />
+         <VenueNfo />
       </div>
     );
   }
