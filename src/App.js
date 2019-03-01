210cdd3c40976ef5172f204eea4fb801a9a6f7f2
xxlxep <xxlxep@gmail.com>
2019-03-01 22:56:42 +0300

    Pricing component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header_footer/Header';
 import Featured from './components/featured';
 import VenueNfo from './components/venueNfo';
 import Highlights from './components/Highlights';
+import Pricing from './components/pricing';
 
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
          <Featured />
          <VenueNfo />
          <Highlights />
+         <Pricing />
       </div>
     );
   }
